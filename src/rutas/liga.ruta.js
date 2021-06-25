3f49ac5610e9e75c7ada9f6c6567b2cd051da909
palvarez2016240 <69530476+palvarez2016240@users.noreply.github.com>
2021-06-25 10:20:36 -0600

    Ruta creada para buscar liga por id

--- src/rutas/liga.ruta.js
+++ src/rutas/liga.ruta.js
@@ -12,5 +12,6 @@ api.post("/Liga", md_autorizacion.ensureAuth, ligaControlador.CrearLiga);
 api.put('/EditarLiga/:id', md_autorizacion.ensureAuth, ligaControlador.EditarLiga);
 api.delete('/EliminarLiga/:id', md_autorizacion.ensureAuth, ligaControlador.EliminarLiga);
 api.get('/ObtenerLigas',ligaControlador.ObterLigas);
+api.get('/ligaId/:idLiga', ligaControlador.ligaId);
 
 module.exports = api;
