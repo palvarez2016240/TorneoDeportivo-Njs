803f3cfd3e56268a9c38c20f233d9616dada8d43
Jlima2016306 <69653451+Jlima2016306@users.noreply.github.com>
2021-06-25 16:52:23 -0600

    rutas de liga con Token

--- src/rutas/liga.ruta.js
+++ src/rutas/liga.ruta.js
@@ -11,7 +11,7 @@ var api = express.Router();
 api.post("/Liga", md_autorizacion.ensureAuth, ligaControlador.CrearLiga);
 api.put('/EditarLiga/:id', md_autorizacion.ensureAuth, ligaControlador.EditarLiga);
 api.delete('/EliminarLiga/:id', md_autorizacion.ensureAuth, ligaControlador.EliminarLiga);
-api.get('/ObtenerLigas',ligaControlador.ObterLigas);
-api.get('/ligaId/:idLiga', ligaControlador.ligaId);
+api.get('/ObtenerLigas',md_autorizacion.ensureAuth,ligaControlador.ObterLigas);
+api.get('/ligaId/:idLiga',md_autorizacion.ensureAuth, ligaControlador.ligaId);
 
 module.exports = api;
