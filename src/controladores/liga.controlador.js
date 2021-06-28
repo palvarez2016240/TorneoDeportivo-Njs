44e831e6fc5894c67549d17696ec58fe90a31cfb
palvarez2016240 <69530476+palvarez2016240@users.noreply.github.com>
2021-06-28 15:13:02 -0600

    Subir imagen hecha

--- src/controladores/liga.controlador.js
+++ src/controladores/liga.controlador.js
@@ -3,6 +3,8 @@
 var User = require('../modelos/usuario.model');
 var Liga = require('../modelos/liga.model');
 var equipo = require('../modelos/equipos.model');
+var fs = require('fs');
+var path = require('path')
 
 
 //  Registrar una liga
@@ -39,6 +41,7 @@ function CrearLiga(req, res) {
                     //Inyectar datos
                     liga.nombres = params.nombres;
                     liga.usuario = idUser;
+                    liga.imagen = null;
 
                     //Guardar las datos
                     liga.save((err, ligaGuardada) => {
@@ -112,9 +115,13 @@ function EditarLiga(req, res) {
 
 //  Eliminar una liga
 function EliminarLiga(req, res) {
+    var params = req.body;
     let LigaId = req.params.id
     var idUsuario;
 
+    //Eliminar el parametro de usuario
+    delete params.usuario;
+    
     //Buscar si la liga existe
     Liga.findOne({ _id: LigaId }).exec((err, ligaEncontrada) => {
         if (err) return res.status(500).send({ mensaje: "Error" });
@@ -155,7 +162,6 @@ function ObterLigas(req, res) {
 }
 
 
-
 //  Buscar liga por id
 function ligaId(req, res) {
     var idLiga = req.params.idLiga;
@@ -178,10 +184,89 @@ function ligaId(req, res) {
 }
 
 
+// Eliminar archivo no apto para imagen
+function eliminarArchivo(res, rutaArchivo, mensaje) {
+
+    //Elimina el archivo no apto 
+    fs.unlink(rutaArchivo, (err) => {
+        return res.status(500).send({ mensaje: mensaje })
+    })
+}
+
+
+//  Subir imagen del equipo
+function subirImagen(req, res) {
+    var idLiga = req.params.idLiga
+
+    //Busqueda para ver si el equipo existe
+    Liga.findOne({ _id: idLiga }).exec((err, ligaEncontrada) => {
+        if (err) return res.status(500).send({ mensaje: "Error" });
+        if (!ligaEncontrada) return res.status(500).send({ mensaje: "La liga no existe" })
+        var idUsuario = ligaEncontrada.usuario;
+
+        //Validar dueño del equipo
+        if (req.user.sub != idUsuario) {
+            return res.status(500).send({ mensaje: "Este equipo no te pertenece" })
+        }
+
+        //Validar que se haya subido un archivo
+        if (req.files) {
+
+            //En esta variable se guardara la ruta de la imagen
+            var direccionArchivo = req.files.imagen.path;
+
+            //Se elimina las diagonales invertidas de la ruta
+            var direccion_split = direccionArchivo.split('\\');
+
+            //En esta variable se guarda el nombre del archivo
+            var nombre_archivo = direccion_split[3];
+
+            //En esta variable se separa el nombre del archivo de su extension  
+            var extension_archivo = nombre_archivo.split('.');
+
+            //Se guarda el nombre de la extension
+            var nombre_extension = extension_archivo[1].toLowerCase();
+
+            //Se valida que la extasion del archivo sea correcta
+            if (nombre_extension === 'png' || nombre_extension === 'jpg' || nombre_extension === 'gif') {
+
+                //Se sube la imagen del equipo
+                Liga.findByIdAndUpdate(idLiga, { imagen: nombre_archivo }, { new: true }, (err, ligaEditada) => {
+                    return res.status(200).send({ ligaEditada });
+                })
+            } else {
+
+                //Se elimina el archivo subido no permitido
+                return eliminarArchivo(res, direccionArchivo, 'Tipo de imagen no permitida');
+            }
+        } else {
+            return res.status(500).send({ mensaje: "No se ha subido ningun archivo" })
+        }
+    })
+}
+
+
+//  Obtener la imagen
+function obtenerImagen(req, res) {
+    var nombreImagen = req.params.imagen;
+    var rutaArchivo = `./src/imagenes/ligas/${nombreImagen}`;
+
+    //Funcion para obtener la imagen en archivo
+    fs.access(rutaArchivo, ((err) => {
+        if (err) {
+            return res.status(500).send({ mensaje: "No existe la imagen" });
+        } else {
+            return res.sendFile(path.resolve(rutaArchivo));
+        }
+    }))
+}
+
 module.exports = {
     CrearLiga,
     EditarLiga,
     EliminarLiga,
     ObterLigas,
-    ligaId
+    ligaId,
+    subirImagen,
+    obtenerImagen
 }
