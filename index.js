a1bee127219c3c302639d84f881d82bd6293f30a
hajbal2019010 <69524425+hajbal2019010@users.noreply.github.com>
2021-07-07 20:36:44 -0600

    Conexion Mongo atals y heroku

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const app = require("./app");
 
 var Admin = require("./src/controladores/usuario.controlador")
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/DBTorneoDeportivo', {useNewUrlParser: true, useUnifiedTopology:true}).then(()=>{
+mongoose.connect('mongodb+srv://admin:admin@dbtorneodeportivo.ors0h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology:true}).then(()=>{
 
     console.log('Se encuentra conectado a la base de datos');
     
     Admin.ADMIN();
     
-    app.listen(3000,function(){
+    app.listen(process.env.Port || 3000,function(){
         console.log('El servidor esta funcionando en el puerto 3000')
     })    
 
